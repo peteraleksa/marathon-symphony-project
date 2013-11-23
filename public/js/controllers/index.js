d9054305f2b59e2f66e0bd55db3ee4342dc10254
peteraleksa <peter.aleksa@gmail.com>
2013-11-22 21:08:06 -0500

    updated views

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,21 +1,29 @@
 angular.module('symphonyApp.system').controller('WelcomeController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 
-    $scope.sidebarModel = 'Top';
-    $scope.sideSort = '-favorites';
-
-    $scope.toggleSidebar = function() {
-
-    	if($scope.sidebarModel == 'Newest') {
-    		$scope.sideSort = '-created';
-    		$scope.sidebarModel = 'Top';
-    	}
-
-    	else if ($scope.sidebarModel == 'Top') {
-    		$scope.sideSort = '-favorites';
-    		$scope.sidebarModel = 'Newest';
-    	}
-
+    $scope.sidebarModel = {
+        top: {
+            sort: '-favorites',
+            selected: true
+        },
+        newest: {
+            sort: '-created',
+            selected: false
+        },
+        getSelected: function() {
+            if(this.top.selected) {
+                return this.top;
+            }
+            else {
+                return this.newest;
+            }
+        }
     };
+
+    $scope.sideSlct = {
+        selected: $scope.sidebarModel.getSelected()
+    }; 
+
+    console.log($scope.sidebarModel.getSelected());
 
 }]);
