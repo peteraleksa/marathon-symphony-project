546b8dc7c89e5b65e8326c1c242a55f02358112d
peteraleksa <peter.aleksa@gmail.com>
2013-11-23 15:32:07 -0500

    finally got the leaderboard to toggle between favorite and newest. turned out to be only 1 line of code with angular-ui-bootstrap

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,29 +1,5 @@
 angular.module('symphonyApp.system').controller('WelcomeController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
-
-    $scope.sidebarModel = {
-        top: {
-            sort: '-favorites',
-            selected: true
-        },
-        newest: {
-            sort: '-created',
-            selected: false
-        },
-        getSelected: function() {
-            if(this.top.selected) {
-                return this.top;
-            }
-            else {
-                return this.newest;
-            }
-        }
-    };
-
-    $scope.sideSlct = {
-        selected: $scope.sidebarModel.getSelected()
-    }; 
-
-    console.log($scope.sidebarModel.getSelected());
+    $scope.sideSort = '-favorites';
 
 }]);
