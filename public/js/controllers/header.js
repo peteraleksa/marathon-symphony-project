c0d03c9071023886846a84a4d847bdd5cb4c80f8
peteraleksa <peter.aleksa@gmail.com>
2013-11-24 01:59:21 -0500

    fixed minor presentation bugs

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -1,26 +1,25 @@
 angular.module('symphonyApp.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
-    $scope.myListLink = "users/" + $scope.global.user._id + "/symphonies";
+    $scope.myListLink = $scope.global.authenticated ? 
+         "users/" + $scope.global.user._id + "/symphonies"
+        : "";
 
     $scope.menu = [{
         "title": "Listen",
         "link": "symphonies/list",
-        "show": "global"
     }, 
     {
         "title": "Compose",
         "link": "symphonies/compose",
-        "show": "global"
     },
     {
     	"title": "About",
     	"link": "about",
-        "show": "global"
-    },
-    {
+    }];
+
+    $scope.authmenu = [{
         "title": "My Symphonies",
         "link": $scope.myListLink,
-        "show": "global.authenticated"
     }];
     
     $scope.isCollapsed = false;
