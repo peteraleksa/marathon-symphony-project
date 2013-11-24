ce71259881cf2a44a92e00efe8430d6cb0c98162
peteraleksa <peter.aleksa@gmail.com>
2013-11-24 11:45:38 -0500

    first attempt at test writing, doesn't really work

--- test/symphony/model.js
+++ test/symphony/model.js
@@ -23,43 +23,55 @@ describe('<Unit Test>', function() {
             });
 
             user.save(function(err) {
-                symphony = new Article({
-                    title: 'Symphony Title',
-                    composer: 'Symphony Composer',
-                    user: user
+                symphony = new Symphony({
+                    created: Date.now,
+                    title: 'Test Title',
+                    composer: 'Test Composer',
+                    year: '2013',
+                    runnerRange: ['Pro Men', 'Pro Women'],
+                    timingLocations {
+                        locations: ['Start', 'Mile 1', 'Mile 2']
+                    },
+                    melody: {
+                        melodyType: 'Harmonic'
+                    }
+                    user: user,
+                    favorites: 0,
+                    shares: 0
                 });
+            });
 
+            done();
+        });
+    });
+
+    describe('Method Save', function() {
+        it('should be able to save without problems', function(done) {
+            return symphony.save(function(err) {
+                should.not.exist(err);
                 done();
             });
         });
 
-        describe('Method Save', function() {
-            it('should be able to save without problems', function(done) {
-                return symphony.save(function(err) {
-                    should.not.exist(err);
-                    done();
-                });
+        it('should be able to show an error when try to save without title', function(done) {
+            symphony.title = '';
+
+            return symphony.save(function(err) {
+                should.exist(err);
+                done();
             });
-
-            it('should be able to show an error when try to save without title', function(done) {
-                article.title = '';
-
-                return symphony.save(function(err) {
-                    should.exist(err);
-                    done();
-                });
-            });
-        });
-
-        afterEach(function(done) {
-            Symphony.remove({});
-            User.remove({});
-            done();
-        });
-        after(function(done){
-            Symphony.remove().exec();
-            User.remove().exec();
-            done();
         });
     });
+
+    afterEach(function(done) {
+        Symphony.remove({});
+        User.remove({});
+        done();
+    });
+    after(function(done){
+        Symphony.remove().exec();
+        User.remove().exec();
+        done();
+    });
 });
+
