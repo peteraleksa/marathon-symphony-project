b7ea61a780a59155f93a1eb156560c8dd8bd7f21
peteraleksa <peter.aleksa@gmail.com>
2013-12-07 17:30:10 -0500

    created race schema to model race data

---
+++ app/models/race.js
@@ -0,0 +1,22 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    config = require('../../config/config'),
+    Schema = mongoose.Schema;
+
+/**
+ * Symphony Schema
+ */
+var RaceSchema = new Schema({
+	year: Number,
+	timingStation: [{
+		location: String,
+		data: [{
+			time: Date,
+			bib: Number
+		}]
+	}]
+});
+
+mongoose.model('Race', RaceSchema);
